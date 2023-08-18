abbfce35c9edac251c6e945f9938fe527b971770
tdg10e <tremaine.grant@gmail.com>
2023-08-18 10:24:30 -0400

    Initial Commit

---
+++ PuppySchool/PuppySchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuppySchoolApp.swift
+//  PuppySchool
+//
+//  Created by Tremaine Grant on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PuppySchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
